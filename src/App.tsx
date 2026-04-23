79a8e204a06990743aa035826bcbee768010f256
JhonnesPossiRibeiro <jh.possi@gmail.com>
2026-04-23 10:41:18 -0300

    component footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon} from 'lucide-react';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
@@ -25,18 +29,27 @@ function App() {
         <Container>
           <form className='form' action=''>
             <div className='formRow'>
-              <DefaultInput id="meuInput" labelText='task' type="text"/>
+              <DefaultInput
+                id='meuInput'
+                labelText='task'
+                type='text'
+                placeholder='Digite algo'
+              />
             </div>
             <div className='formRow'>
               <p>Lorem, ipsum dolor sit amet.</p>
             </div>
             <div className='formRow'>
-              
+              <Cycles />
             </div>
             <div className='formRow'>
-              <button>Enviar</button>
+              <DefaultButton icon={<PlayCircleIcon />} />
             </div>
           </form>
+        </Container>
+
+        <Container>
+          <Footer/>
         </Container>
       </div>
     </>
