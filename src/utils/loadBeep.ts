fa4c2c35228013b3e613376d54535edba1fca9d4
JhonnesPossiRibeiro <jh.possi@gmail.com>
2026-04-28 16:04:26 -0300

    audio

---
+++ src/utils/loadBeep.ts
@@ -0,0 +1,11 @@
+import gratationBeep from '../assets/audios/gravitational_beep.mp3';
+
+export function loadBeep() {
+  const audio = new Audio(gratationBeep);
+  audio.load();
+
+  return () => {
+    audio.currentTime = 0;
+    audio.play().catch(error => console.log('Erro ao tocar o audio', error));
+  };
+}
