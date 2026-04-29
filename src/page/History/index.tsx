71ab79c36a98c24b1acc2122db2f22a30048432b
JhonnesPossiRibeiro <jh.possi@gmail.com>
2026-04-29 16:47:04 -0300

    limpando histórico

--- src/page/History/index.tsx
+++ src/page/History/index.tsx
@@ -1,4 +1,4 @@
-import { Trash2Icon } from 'lucide-react';
+import { Bold, Trash2Icon } from 'lucide-react';
 import { Container } from '../../components/Container';
 import { DefaultButton } from '../../components/DefaultButton';
 import { Heading } from '../../components/Heading';
@@ -7,11 +7,14 @@ import styles from './styles.module.css';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { formatDate } from '../../utils/formatDate';
 import { getTaskStatus } from '../../utils/getTaskStatus';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { sortTasks, type SortTasksOptions } from '../../utils/sortTasks';
+import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
 
 export function History() {
-  const { state } = useTaskContext();
+  const { state, dispatch } = useTaskContext();
+  const hasTasks = state.tasks.length > 0;
+
   const [sortTaskOptions, setSortTaskOptions] = useState<SortTasksOptions>(
     () => {
       return {
@@ -21,6 +24,17 @@ export function History() {
       };
     },
   );
+
+  useEffect(() => {
+    setSortTaskOptions(prevState => ({
+      ...prevState,
+      tasks: sortTasks({
+        tasks: state.tasks,
+        direction: prevState.direction,
+        field: prevState.field,
+      }),
+    }));
+  }, [state.tasks]);
 
   function handleSortTasks({ field }: Pick<SortTasksOptions, 'field'>) {
     const newDirection = sortTaskOptions.direction === 'desc' ? 'asc' : 'desc';
@@ -35,56 +49,87 @@ export function History() {
     });
   }
 
+  function handleResetHistory() {
+    if (!confirm('Tem certeza')) return;
+
+    dispatch({ type: TaskActionTypes.RESET_STATE });
+  }
+
   return (
     <>
       <MainTemplate>
         <Container>
           <Heading>
             <span>Histórico</span>
-            <span className={styles.buttonContainer}>
-              <DefaultButton
-                icon={<Trash2Icon />}
-                color='red'
-                aria-label='Apagar todo o histórico'
-                title='Apagar histórico'
-              />
-            </span>
+            {hasTasks && (
+              <span className={styles.buttonContainer}>
+                <DefaultButton
+                  icon={<Trash2Icon />}
+                  color='red'
+                  aria-label='Apagar todo o histórico'
+                  title='Apagar histórico'
+                  onClick={handleResetHistory}
+                />
+              </span>
+            )}
           </Heading>
         </Container>
 
         <Container>
-          <div className={styles.responsiveTable}>
-            <table>
-              <thead>
-                <tr>
-                  <th  onClick={()=> handleSortTasks({field: 'name'})} className={styles.thSort}>Tarefa ⇅</th>
-                  <th  onClick={()=> handleSortTasks({field: 'duration'})} className={styles.thSort}>Duração ⇅</th>
-                  <th  onClick={()=> handleSortTasks({field: 'startDate'})} className={styles.thSort}>Data ⇅</th>
-                  <th>Status</th>
-                  <th>Tipo</th>
-                </tr>
-              </thead>
-              <tbody>
-                {sortTaskOptions.tasks.map(task => {
-                  const taskTypeDictionary = {
-                    workTime: 'Foco',
-                    shortBreakTime: 'Descanso curto',
-                    longBreakTime: 'Descanso longo',
-                  };
+          {hasTasks && (
+            <div className={styles.responsiveTable}>
+              <table>
+                <thead>
+                  <tr>
+                    <th
+                      onClick={() => handleSortTasks({ field: 'name' })}
+                      className={styles.thSort}
+                    >
+                      Tarefa ⇅
+                    </th>
+                    <th
+                      onClick={() => handleSortTasks({ field: 'duration' })}
+                      className={styles.thSort}
+                    >
+                      Duração ⇅
+                    </th>
+                    <th
+                      onClick={() => handleSortTasks({ field: 'startDate' })}
+                      className={styles.thSort}
+                    >
+                      Data ⇅
+                    </th>
+                    <th>Status</th>
+                    <th>Tipo</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {sortTaskOptions.tasks.map(task => {
+                    const taskTypeDictionary = {
+                      workTime: 'Foco',
+                      shortBreakTime: 'Descanso curto',
+                      longBreakTime: 'Descanso longo',
+                    };
 
-                  return (
-                    <tr key={task.id}>
-                      <td>{task.name}</td>
-                      <td>{task.duration}min</td>
-                      <td>{formatDate(task.startDate)}</td>
-                      <td>{getTaskStatus(task, state.activeTask)}</td>
-                      <td>{taskTypeDictionary[task.type]}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
+                    return (
+                      <tr key={task.id}>
+                        <td>{task.name}</td>
+                        <td>{task.duration}min</td>
+                        <td>{formatDate(task.startDate)}</td>
+                        <td>{getTaskStatus(task, state.activeTask)}</td>
+                        <td>{taskTypeDictionary[task.type]}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          )}
+          {!hasTasks && (
+            <p style={{ textAlign: 'center', fontWeight: 'bold' }}>
+              Ainda não existem tarefas criadas.
+            </p>
+          )}
         </Container>
       </MainTemplate>
     </>
