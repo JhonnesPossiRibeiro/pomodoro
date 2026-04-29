c4b5e58e930619ce4caa590c1517722606fd0faa
JhonnesPossiRibeiro <jh.possi@gmail.com>
2026-04-29 11:33:59 -0300

    lastTask e title

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -14,6 +14,7 @@ import { showMessage } from '../../adapters/showMessage';
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
   const tsakNameInput = useRef<HTMLInputElement>(null);
+  const lastTaskName = state.tasks[state.tasks.length -1]?.name || '';
 
   const nextCycle = getNextCycle(state.currentCycle);
   const nextCycleType = getNextCycleType(nextCycle);
@@ -62,6 +63,7 @@ export function MainForm() {
           placeholder='Digite algo'
           ref={tsakNameInput}
           disabled={!!state.activeTask}
+          defaultValue={lastTaskName}
         />
       </div>
       <div className='formRow'>
